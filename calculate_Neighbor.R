f3f3624cf61cd066ef4b6326ada5303aa24e0faa
quxingan <quxingan999@126.com>
2022-03-31 20:47:04 +0800

    modify flexscan function, modify china data
    add method param, fill nas in china

--- calculate_Neighbor.R
+++ calculate_Neighbor.R
@@ -33,16 +33,41 @@ district_summary$expected_case = as.integer(10000*runif(dim(district_summary)[1]
 
 shp_dat = china@data
 
-new_china_data = sqldf("select  a.dt_adcode, case_cnt,expected_case,
-                        lon,lat
+new_china_data = sqldf("select  a.dt_adcode, b.case_cnt,b.expected_case
+                        
                        from shp_dat a
                        left join district_summary b 
                        on a.dt_adcode = b.code
                        ")
-new_china_data$lon = as.numeric(new_china_data$lon)
-new_china_data$lat = as.numeric(new_china_data$lat)
+new_china_data2 = sqldf('select dt_adcode, a.case_cnt, a.expected_case,
+                       b.lon,b.lat
+                       from
+                       new_china_data a
+                       left join region b
+                       on a.dt_adcode= cast(b.code as varchar)')
 
-#remove na
+new_china_data2$lon = as.numeric(new_china_data2$lon)
+new_china_data2$lat = as.numeric(new_china_data2$lat)
+
+na.dat = new_china_data2[is.na(new_china_data2$lon),]
+#1st method fill na
+na.pos = seq(1:length(new_china_data2$lon))[is.na(new_china_data2$lon)]
+poly_list = china@polygons
+for(j in 1:length(na.pos)){
+  i = na.pos[j]
+  a = poly_list[[i]]@Polygons
+  b = a[[1]]@coords
+  fill_lon = mean(b[,1])
+  fill_lat = mean(b[,2])
+  new_china_data2[i,]$lon = fill_lon
+  new_china_data2[i,]$lat = fill_lat
+}
+new_china_data2$case_cnt[is.na(new_china_data2$case_cnt)] = 0
+new_china_data2$expected_case[is.na(new_china_data2$expected_case)] = 5000
+china@data = new_china_data2
+
+
+#2st method remove na
 na.pos = is.na(new_china_data$lon)
 new_china_data = new_china_data[!na.pos,]
 china@data = new_china_data
@@ -56,7 +81,7 @@ o = o[is.na(match.pos)]
 china@plotOrder=o
 
 #run algorithm
-res = flexscan_qxa(china)
+res = flexscan_qxa(china,'coord')
 
 
 
--- flexscan.R
+++ flexscan.R
@@ -1,13 +1,23 @@
 #calculate fls
 #province_shp: .shp data
-flexscan_qxa = function(province_shp){
+#method: the way to calculate coord
+flexscan_qxa = function(province_shp,method){
   nb <- poly2nb(province_shp, queen = T)
-  coord <- data.frame(x=province_shp$lon, y=province_shp$lat)
-  coordinates(coord) <- c("x", "y")
-  proj4string(coord) <- proj4string(province_shp)
-  coord <- spTransform(coord, CRS("+init=epsg:3415"))
-  fls <- rflexscan(name = province_shp$dt_adcode,
-                   x = coord$x, y = coord$y, nb = nb,
-                   observed = province_shp$case_cnt, expected = province_shp$expected_case)
+  if(method=='original'){
+    fls <- rflexscan(name = province_shp$dt_adcode,
+                     lon = province_shp$lon, lat = province_shp$lat, nb = nb,
+                     observed = province_shp$case_cnt, expected = province_shp$expected_case)
+  }
+  else if(method=='coord')
+  {
+    coord <- data.frame(x=province_shp$lon, y=province_shp$lat)
+    coordinates(coord) <- c("x", "y")
+    proj4string(coord) <- proj4string(province_shp)
+    coord <- spTransform(coord, CRS("+init=epsg:3415"))
+    fls <- rflexscan(name = province_shp$dt_adcode,
+                     x = coord$x, y = coord$y, nb = nb,
+                     observed = province_shp$case_cnt, expected = province_shp$expected_case)
+  }
+
   list(fls,nb)
 }
